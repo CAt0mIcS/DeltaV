c94e48fa9bb28a856d7de98558fbb5afd7778143
CAt0mIcS <c.simon.geier@gmail.com>
2026-08-16 13:11:21 +0200

    Primitive control algorithm

--- electrical/Stepper Motor Driver/tests/H-Bridge Arduino Testing Circuit/src/main.cpp
+++ electrical/Stepper Motor Driver/tests/H-Bridge Arduino Testing Circuit/src/main.cpp
@@ -16,11 +16,24 @@ static int s_CosineValues[] = {127, 126, 126, 125, 124, 122, 121, 119, 116, 114,
 #define PIN_MOS_RIGHTB_HIGH 26
 #define PIN_MOS_RIGHTB_LOW 27
 
-#define PIN_CURRENT_SENSOR A3
+#define PIN_CURRENT_SENSORA A3
+#define PIN_CURRENT_SENSORB A4
 
 static int s_StepCount = 0;
 
 void incrementStepCount();
+void printCurrentmA(const char *text, float current);
+void printVoltagemV(const char *text, float voltage);
+
+void enableForwardA();
+void enableBackwardA();
+void enableForwardB();
+void enableBackwardB();
+
+void disableForwardA();
+void disableBackwardA();
+void disableForwardB();
+void disableBackwardB();
 
 void setup()
 {
@@ -33,14 +46,58 @@ void setup()
     pinMode(PIN_MOS_LEFTB_LOW, OUTPUT);
     pinMode(PIN_MOS_RIGHTB_HIGH, OUTPUT);
     pinMode(PIN_MOS_RIGHTB_LOW, OUTPUT);
+
+    Serial.begin(9600);
 }
 
 void loop()
 {
-    int currentCurrent = analogRead(PIN_CURRENT_SENSOR);
+    float voltagemVA = analogRead(PIN_CURRENT_SENSORA) * 5000.f / 1023.f;
+    float currentmAA = (voltagemVA - 2500.f) / 185.f;
 
-    int coilATarget = s_SineValues[s_StepCount] / 127.f * I_MAX;
-    int coilBTarget = s_CosineValues[s_StepCount] / 127.f * I_MAX;
+    printCurrentmA("Current current A: ", currentmAA);
+
+    float voltagemVB = analogRead(PIN_CURRENT_SENSORB) * 5000.f / 1023.f;
+    float currentmAB = (voltagemVB - 2500.f) / 185.f;
+
+    printCurrentmA("Current current B: ", currentmAA);
+
+    float coilATarget = s_SineValues[s_StepCount] / 127.f * I_MAX;
+    float coilBTarget = s_CosineValues[s_StepCount] / 127.f * I_MAX;
+    printCurrentmA("Target current A: ", coilATarget);
+    printCurrentmA("Target current B: ", coilBTarget);
+
+    if (coilATarget >= 0.f)
+    {
+        if (currentmAA < coilATarget)
+            enableForwardA();
+        else if (currentmAA > coilATarget)
+            disableForwardA();
+    }
+    else
+    {
+        if (currentmAA > coilATarget)
+            enableBackwardA();
+        else if (currentmAA < coilATarget)
+            disableBackwardA();
+    }
+
+    if (coilBTarget >= 0.f)
+    {
+        if (currentmAB < coilBTarget)
+            enableForwardB();
+        if (currentmAB > coilBTarget)
+            disableForwardB();
+    }
+    else
+    {
+        if (currentmAB > coilBTarget)
+            enableBackwardB();
+        else if (currentmAB < coilBTarget)
+            disableBackwardB();
+    }
+
+    delay(1);
 }
 
 void incrementStepCount()
@@ -48,4 +105,74 @@ void incrementStepCount()
     s_StepCount++;
     if (s_StepCount >= ANGLE_ARRAY_SIZE)
         s_StepCount = 0;
+}
+
+void enableForwardA()
+{
+    digitalWrite(PIN_MOS_LEFTA_LOW, LOW);
+    digitalWrite(PIN_MOS_RIGHTA_HIGH, LOW);
+    digitalWrite(PIN_MOS_LEFTA_HIGH, HIGH);
+    digitalWrite(PIN_MOS_RIGHTA_LOW, HIGH);
+}
+
+void enableBackwardA()
+{
+    digitalWrite(PIN_MOS_LEFTA_HIGH, LOW);
+    digitalWrite(PIN_MOS_RIGHTA_LOW, LOW);
+    digitalWrite(PIN_MOS_LEFTA_LOW, HIGH);
+    digitalWrite(PIN_MOS_RIGHTA_HIGH, HIGH);
+}
+
+void enableForwardB()
+{
+    digitalWrite(PIN_MOS_LEFTB_LOW, LOW);
+    digitalWrite(PIN_MOS_RIGHTB_HIGH, LOW);
+    digitalWrite(PIN_MOS_LEFTB_HIGH, HIGH);
+    digitalWrite(PIN_MOS_RIGHTB_LOW, HIGH);
+}
+
+void enableBackwardB()
+{
+    digitalWrite(PIN_MOS_LEFTB_HIGH, LOW);
+    digitalWrite(PIN_MOS_RIGHTB_LOW, LOW);
+    digitalWrite(PIN_MOS_LEFTB_LOW, HIGH);
+    digitalWrite(PIN_MOS_RIGHTB_HIGH, HIGH);
+}
+
+void disableForwardA()
+{
+    digitalWrite(PIN_MOS_LEFTA_HIGH, LOW);
+    digitalWrite(PIN_MOS_RIGHTA_LOW, LOW);
+}
+
+void disableBackwardA()
+{
+    digitalWrite(PIN_MOS_LEFTA_LOW, LOW);
+    digitalWrite(PIN_MOS_RIGHTA_HIGH, LOW);
+}
+
+void disableForwardB()
+{
+    digitalWrite(PIN_MOS_LEFTB_HIGH, LOW);
+    digitalWrite(PIN_MOS_RIGHTB_LOW, LOW);
+}
+
+void disableBackwardB()
+{
+    digitalWrite(PIN_MOS_LEFTB_LOW, LOW);
+    digitalWrite(PIN_MOS_RIGHTB_HIGH, LOW);
+}
+
+void printCurrentmA(const char *text, float current)
+{
+    Serial.print(text);
+    Serial.print(current);
+    Serial.println(" mA");
+}
+
+void printVoltagemV(const char *text, float voltage)
+{
+    Serial.print(text);
+    Serial.print(voltage);
+    Serial.println(" mV");
 }
