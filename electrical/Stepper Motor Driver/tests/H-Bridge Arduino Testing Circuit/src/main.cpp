73e427c1a99edd77a058ac548a8d3b1f830ddf7d
CAt0mIcS <c.simon.geier@gmail.com>
2026-08-25 16:20:54 +0200

    PWM

--- electrical/Stepper Motor Driver/tests/H-Bridge Arduino Testing Circuit/src/main.cpp
+++ electrical/Stepper Motor Driver/tests/H-Bridge Arduino Testing Circuit/src/main.cpp
@@ -6,15 +6,10 @@ static int s_CosineValues[] = {127, 126, 126, 125, 124, 123, 121, 119, 117, 114,
 #define ANGLE_ARRAY_SIZE 128
 #define I_MAX 500
 
-#define PIN_MOS_LEFTA_HIGH 25
-#define PIN_MOS_LEFTA_LOW 24
-#define PIN_MOS_RIGHTA_HIGH 22
+#define PIN_MOS_LEFTA_HIGH 2
+#define PIN_MOS_LEFTA_LOW 22
+#define PIN_MOS_RIGHTA_HIGH 3
 #define PIN_MOS_RIGHTA_LOW 23
-
-#define PIN_MOS_LEFTB_HIGH 29
-#define PIN_MOS_LEFTB_LOW 28
-#define PIN_MOS_RIGHTB_HIGH 26
-#define PIN_MOS_RIGHTB_LOW 27
 
 #define PIN_CURRENT_SENSORA A3
 #define PIN_CURRENT_SENSORB A4
@@ -40,25 +35,62 @@ void disableBackwardB();
 
 void stepButtonPressed();
 
+void setDutyCycle(int pin, float percentage);
 void chargeForwardA();
 
 void setup()
 {
-    pinMode(PIN_MOS_LEFTA_HIGH, OUTPUT);
-    pinMode(PIN_MOS_LEFTA_LOW, OUTPUT);
+    // Timer 1 pins: OC1A (Pin 11), OC1B (Pin 12)
+    // pinMode(11, OUTPUT);
+    // pinMode(12, OUTPUT);
+
+    // Timer 3 pins: OC3A (Pin 5), OC3B (Pin 2), OC3C (Pin 3)
     pinMode(PIN_MOS_RIGHTA_HIGH, OUTPUT);
+    pinMode(PIN_MOS_LEFTA_HIGH, OUTPUT);
+    // pinMode(5, OUTPUT);
+
+    // Timer 4 pins: OC4A (Pin 6), OC4B (Pin 7), OC4C (Pin 8)
+    // pinMode(6, OUTPUT);
+    // pinMode(7, OUTPUT);
+    // pinMode(8, OUTPUT);
+
+    // Timer 5 pins: OC5A (Pin 46), OC5B (Pin 45), OC5C (Pin 44)
+    // pinMode(46, OUTPUT);
+    // pinMode(45, OUTPUT);
+    // pinMode(44, OUTPUT);
+
+    // Change timer 1,3,4,5 (16-bit timers) to Fast PWM and 25kHz
+    // TIMER 1
+    // TCCR1A = _BV(COM1A1) | _BV(COM1B1) | _BV(WGM11);
+    // TCCR1B = _BV(WGM13) | _BV(WGM12) | _BV(CS10);
+    // ICR1   = 319;
+
+    // TIMER 3
+    TCCR3A = _BV(COM3A1) | _BV(COM3B1) | _BV(COM3C1) | _BV(WGM31);
+    TCCR3B = _BV(WGM33) | _BV(WGM32) | _BV(CS30);
+    ICR3 = 319;
+
+    // TIMER 4
+    // TCCR4A = _BV(COM4A1) | _BV(COM4B1) | _BV(COM4C1) | _BV(WGM41);
+    // TCCR4B = _BV(WGM43) | _BV(WGM42) | _BV(CS40);
+    // ICR4 = 319;
+
+    // TIMER 5
+    // TCCR5A = _BV(COM5A1) | _BV(COM5B1) | _BV(COM5C1) | _BV(WGM51);
+    // TCCR5B = _BV(WGM53) | _BV(WGM52) | _BV(CS50);
+    // ICR5   = 319;
+
+    pinMode(PIN_MOS_LEFTA_LOW, OUTPUT);
     pinMode(PIN_MOS_RIGHTA_LOW, OUTPUT);
 
     Serial.begin(9600);
 
     delay(100);
 
-    chargeForwardA();
-    delay(15);
-    digitalWrite(PIN_MOS_RIGHTA_LOW, LOW);
-    delay(1);
+    digitalWrite(PIN_MOS_RIGHTA_LOW, HIGH); // Slow decay for forward direction
 
-    digitalWrite(PIN_MOS_LEFTA_HIGH, HIGH);
+    setDutyCycle(PIN_MOS_RIGHTA_HIGH, 0);
+    setDutyCycle(PIN_MOS_LEFTA_HIGH, 10);
 }
 
 static int counter = 0;
@@ -98,29 +130,29 @@ void enableBackwardA()
     digitalWrite(PIN_MOS_RIGHTA_HIGH, HIGH);
 }
 
-void enableForwardB()
-{
-    digitalWrite(PIN_MOS_LEFTB_LOW, LOW);
-    digitalWrite(PIN_MOS_RIGHTB_HIGH, LOW);
+// void enableForwardB()
+// {
+//     digitalWrite(PIN_MOS_LEFTB_LOW, LOW);
+//     digitalWrite(PIN_MOS_RIGHTB_HIGH, LOW);
 
-    // Deadtime
-    delayMicroseconds(1);
+//     // Deadtime
+//     delayMicroseconds(1);
 
-    digitalWrite(PIN_MOS_LEFTB_HIGH, HIGH);
-    digitalWrite(PIN_MOS_RIGHTB_LOW, HIGH);
-}
+//     digitalWrite(PIN_MOS_LEFTB_HIGH, HIGH);
+//     digitalWrite(PIN_MOS_RIGHTB_LOW, HIGH);
+// }
 
-void enableBackwardB()
-{
-    digitalWrite(PIN_MOS_LEFTB_HIGH, LOW);
-    digitalWrite(PIN_MOS_RIGHTB_LOW, LOW);
+// void enableBackwardB()
+// {
+//     digitalWrite(PIN_MOS_LEFTB_HIGH, LOW);
+//     digitalWrite(PIN_MOS_RIGHTB_LOW, LOW);
 
-    // Deadtime
-    delayMicroseconds(1);
+//     // Deadtime
+//     delayMicroseconds(1);
 
-    digitalWrite(PIN_MOS_LEFTB_LOW, HIGH);
-    digitalWrite(PIN_MOS_RIGHTB_HIGH, HIGH);
-}
+//     digitalWrite(PIN_MOS_LEFTB_LOW, HIGH);
+//     digitalWrite(PIN_MOS_RIGHTB_HIGH, HIGH);
+// }
 
 void disableForwardA()
 {
@@ -134,17 +166,17 @@ void disableBackwardA()
     digitalWrite(PIN_MOS_RIGHTA_HIGH, LOW);
 }
 
-void disableForwardB()
-{
-    digitalWrite(PIN_MOS_LEFTB_HIGH, LOW);
-    digitalWrite(PIN_MOS_RIGHTB_LOW, LOW);
-}
+// void disableForwardB()
+// {
+//     digitalWrite(PIN_MOS_LEFTB_HIGH, LOW);
+//     digitalWrite(PIN_MOS_RIGHTB_LOW, LOW);
+// }
 
-void disableBackwardB()
-{
-    digitalWrite(PIN_MOS_LEFTB_LOW, LOW);
-    digitalWrite(PIN_MOS_RIGHTB_HIGH, LOW);
-}
+// void disableBackwardB()
+// {
+//     digitalWrite(PIN_MOS_LEFTB_LOW, LOW);
+//     digitalWrite(PIN_MOS_RIGHTB_HIGH, LOW);
+// }
 
 void stepButtonPressed()
 {
@@ -153,6 +185,21 @@ void stepButtonPressed()
     incrementStep(&s_StepCount);
     Serial.print("New step target: ");
     Serial.println(s_StepCount);
+}
+
+void setDutyCycle(int pin, float percentage)
+{
+    int dutyVal = (int)(percentage * 319.f);
+
+    if (pin == PIN_MOS_LEFTA_HIGH)
+        OCR3B = dutyVal;
+    else if (pin == PIN_MOS_RIGHTA_HIGH)
+        OCR3C = dutyVal;
+    else
+    {
+        Serial.print("Wrong pin in setDutyCycle: ");
+        Serial.println(pin);
+    }
 }
 
 void chargeForwardA()
