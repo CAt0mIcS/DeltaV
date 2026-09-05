15510eb02e84008d72510695dbd8f6361410e96c
CAt0mIcS <c.simon.geier@gmail.com>
2026-09-05 14:00:57 +0200

    Incrementing target step according to microstepping interval

--- electrical/Stepper-Motor-Driver/FPGA-Controller/src/input_step.v
+++ electrical/Stepper-Motor-Driver/FPGA-Controller/src/input_step.v
@@ -1,6 +1,10 @@
+`include "params.vh"
+
+
 module input_step (
         input i_step,
-        output reg [7:0] o_target_step
+        input [2:0] i_microstepping_interval,
+        output reg [9:0] o_target_step
     );
 
     initial begin
@@ -8,7 +12,26 @@ module input_step (
     end
 
     always @(posedge i_step) begin
-        o_target_step <= o_target_step + 1;
+        case(i_microstepping_interval)
+            `MICROSTEPPING_INTERVAL_1:
+                o_target_step <= o_target_step + `MICROSTEPPING_INCREMENT_1;
+            `MICROSTEPPING_INTERVAL_1_4:
+                o_target_step <= o_target_step + `MICROSTEPPING_INCREMENT_1_4;
+            `MICROSTEPPING_INTERVAL_1_8:
+                o_target_step <= o_target_step + `MICROSTEPPING_INCREMENT_1_8;
+            `MICROSTEPPING_INTERVAL_1_16:
+                o_target_step <= o_target_step + `MICROSTEPPING_INCREMENT_1_16;
+            `MICROSTEPPING_INTERVAL_1_32:
+                o_target_step <= o_target_step + `MICROSTEPPING_INCREMENT_1_32;
+            `MICROSTEPPING_INTERVAL_1_64:
+                o_target_step <= o_target_step + `MICROSTEPPING_INCREMENT_1_64;
+            `MICROSTEPPING_INTERVAL_1_128:
+                o_target_step <= o_target_step + `MICROSTEPPING_INCREMENT_1_128;
+            `MICROSTEPPING_INTERVAL_1_256:
+                o_target_step <= o_target_step + `MICROSTEPPING_INCREMENT_1_256;
+            default:
+                o_target_step <= o_target_step + `MICROSTEPPING_INCREMENT_1;
+        endcase
     end
 
 endmodule
--- electrical/Stepper-Motor-Driver/FPGA-Controller/sym/input_step_tb.v
+++ electrical/Stepper-Motor-Driver/FPGA-Controller/sym/input_step_tb.v
@@ -1,14 +1,18 @@
 // `timescale 1850ps / 10ps
 `timescale 10ns / 100ps
 
+`include "params.vh"
+
 module input_step_tb;
 
     reg step;
-    wire [7:0] target_step;
-    input_step dut(.i_step(step), .o_target_step(target_step));
+    wire [9:0] target_step;
+    reg [2:0] microstepping_interval;
+    input_step dut(.i_step(step), .i_microstepping_interval(microstepping_interval), .o_target_step(target_step));
 
     initial begin
         step = 0;
+        microstepping_interval = `MICROSTEPPING_INTERVAL_1;
 
         $dumpfile("waveform.vcd");
         $dumpvars(0, input_step_tb);
@@ -17,8 +21,11 @@ module input_step_tb;
     end
 
     always begin
-        #10
+        #7
          step = ~step;
+
+        #17
+         microstepping_interval <= microstepping_interval + 1;
     end
 
 endmodule
